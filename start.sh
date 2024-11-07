264ec91c7802afcfc84248b56b510d6f60728eda
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2024-11-07 21:24:44 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+# Don't Remove Credit @
+# Subscribe YouTube Channel For Amazing Bot @
+# Ask Doubt on telegram @
+
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Itssaravanakrish/XnxxWorkBot.git /XnxxWorkBot 
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /XnxxWorkBot 
+fi
+cd /XnxxWorkBot 
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
